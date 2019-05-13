23fa58653becb5621920d6324c7e0b03a503f294
erikchan103 <erik.chan@sugarmade.com>
2019-05-13 10:35:42 -0700

    Updates

---
+++ precache-manifest.fa15c859b86dd2ad6de710db9902d64e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e762a48091ceb80200877afe6c6bb26",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "faa409b038dfb73efcb9",
+    "url": "/clickyGame/static/css/main.97825b9c.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/clickyGame/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "faa409b038dfb73efcb9",
+    "url": "/clickyGame/static/js/main.8320a654.chunk.js"
+  },
+  {
+    "revision": "95f5ce002aae55a30c31",
+    "url": "/clickyGame/static/js/runtime~main.d5f62368.js"
+  }
+]);
